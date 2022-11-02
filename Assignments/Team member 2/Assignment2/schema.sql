a6601177bd5460a55a2e3d6be7b5f8fde3b0dcae
SanthanaSelvi <114221952+SanthanaSelvi@users.noreply.github.com>
2022-11-02 18:50:44 +0530

    Add files via upload

---
+++ Assignments/Team member 2/Assignment2/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+INSERT INTO users (username, password, name, email) VALUES (
+    "user6",
+    "user7@123",
+    "user",
+    "user@gmail.com"
+);
