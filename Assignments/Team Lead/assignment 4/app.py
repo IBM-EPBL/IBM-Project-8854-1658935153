4cc609ae011e853973a7879c1c4b959a591117b8
Rakseda <85486472+Rakseda@users.noreply.github.com>
2022-11-10 13:18:23 +0530

    Add files via upload

---
+++ Assignments/Team Lead/assignment 4/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506'
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            name = "Shyam Mohan"
+            return redirect(url_for('index', messages=name ))
+
+    return render_template('create.html')
+
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
+@app.route('/')
+def index():
+    return render_template('index.html', messages=messages)
+
